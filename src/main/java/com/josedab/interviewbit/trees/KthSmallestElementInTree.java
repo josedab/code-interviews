fa6c869a3f4e666a7b862a2477c588ecc43d8606
Jose David Baena <josedab@gmail.com>
2018-04-20 19:19:02 -0700

    InterviewBit: Trees: KthSmallesElementInTree

---
+++ src/main/java/com/josedab/interviewbit/trees/KthSmallestElementInTree.java
@@ -0,0 +1,71 @@
+package com.josedab.interviewbit.trees;
+
+import com.josedab.interviewbit.model.TreeNode;
+import com.josedab.interviewbit.utils.TreeNodeUtils;
+
+/**
+ * Given a binary search tree, write a function to find the kth smallest element in the tree.
+
+ Example :
+
+ Input :
+    2
+   / \
+  1   3
+
+ and k = 2
+
+ Return : 2
+
+ As 2 is the second smallest element in the tree.
+ NOTE : You may assume 1 <= k <= Total number of nodes in BST
+
+ Official solution
+ For improved solution see @{@link KthSmallestElementInTreeAlternative}
+ */
+public class KthSmallestElementInTree {
+    public int kthsmallest(TreeNode root, int nthLowest) {
+        if (root == null) {
+            return 1;
+        }
+        int nLeftElements = elementsInTree(root.left);
+        if (nthLowest > nLeftElements) {
+            if (nthLowest == nLeftElements + 1) {
+                return root.val;
+            } else {
+                return kthsmallest(root.right, nthLowest - nLeftElements - 1);
+            }
+
+        } else if (nthLowest <= nLeftElements) {
+            return kthsmallest(root.left, nthLowest);
+
+        }
+        return -1;
+    }
+
+    public int elementsInTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int result = 0;
+        if (root.left != null) {
+            result += elementsInTree(root.left) + 1;
+        }
+
+        if (root.right != null) {
+            result += elementsInTree(root.right) + 1;
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        KthSmallestElementInTree solution = new KthSmallestElementInTree();
+        TreeNode problem = TreeNodeUtils.createTreeNodeListFromPreOrderSequence(2, 1, 3);
+        System.out.println(solution.kthsmallest(problem, 2));
+    }
+}
---
+++ src/main/java/com/josedab/interviewbit/trees/KthSmallestElementInTreeAlternative.java
@@ -0,0 +1,79 @@
+package com.josedab.interviewbit.trees;
+
+import com.josedab.interviewbit.model.TreeNode;
+import com.josedab.interviewbit.utils.TreeNodeUtils;
+
+/**
+ * Given a binary search tree, write a function to find the kth smallest element in the tree.
+
+ Example :
+
+ Input :
+    2
+   / \
+  1   3
+
+ and k = 2
+
+ Return : 2
+
+ As 2 is the second smallest element in the tree.
+ NOTE : You may assume 1 <= k <= Total number of nodes in BST
+
+ Solution proposed. I think it is better than official one as it goes through the elements only once and stops
+ once it finds the element.
+ In solution @{@link KthSmallestElementInTree} there are edge cases where they go through more elements than necessary
+ and they dont't take advantage of the inherent properties of a BST.
+
+ TODO:
+ Work on a benchmark
+ */
+public class KthSmallestElementInTreeAlternative {
+
+    private class Counter {
+        int count = 1;
+
+        public Counter(int count) {
+            this.count = count;
+        }
+
+        public int increaseCounter() {
+            this.count++;
+            return this.count;
+        }
+
+    }
+
+    public int kthsmallest(TreeNode treeNode, int nthIndex) {
+        Counter c = new Counter(1);
+        return kthsmallest(treeNode, c, nthIndex);
+    }
+
+    private int kthsmallest(TreeNode treeNode, Counter counter, int nthIndex) {
+        int result = -1;
+        if (treeNode != null) {
+
+            if (treeNode.left != null) {
+                result = kthsmallest(treeNode.left, counter, nthIndex);
+            }
+
+            int count = counter.increaseCounter();
+            if (result != -1) {
+                return result;
+            } else if (count == nthIndex) {
+                return treeNode.val;
+            }
+
+            if (treeNode.right != null) {
+                result = kthsmallest(treeNode.right, counter, nthIndex);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        KthSmallestElementInTreeAlternative solution = new KthSmallestElementInTreeAlternative();
+        TreeNode problem = TreeNodeUtils.createTreeNodeListFromPreOrderSequence(2, 1, 3);
+        System.out.println(solution.kthsmallest(problem, 2));
+    }
+}
